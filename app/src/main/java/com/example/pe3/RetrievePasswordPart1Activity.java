fd4432ad5a866eae66613f8072cba3bfc18a5cd7
LI ZILONG <591122636@qq.com>
2022-05-30 16:02:45 +0800

    Li_2

---
+++ app/src/main/java/com/example/pe3/RetrievePasswordPart1Activity.java
@@ -0,0 +1,14 @@
+package com.example.pe3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetrievePasswordPart1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_password_part1);
+    }
+}
