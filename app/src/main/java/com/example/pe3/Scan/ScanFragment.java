4457a7bb189ea29eb435dc81eef870e813d9c626
LI ZILONG <591122636@qq.com>
2022-07-06 11:03:35 +0800

    123

--- app/src/main/java/com/example/pe3/Scan/ScanFragment.java
+++ app/src/main/java/com/example/pe3/Scan/ScanFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,7 +32,7 @@ import java.util.LinkedList;
 public class ScanFragment extends Fragment {
 
 
-    Button btnScan;
+    ImageButton btnScan;
     TextView tv_qr_readTxt;
 
     private RecyclerView recyclerView;
